da35d727f9ceaaa6f5df795dc69f9223fa3e292f
robothaver <robothaver109@gmail.com>
2025-09-21 12:31:08 +0200

    Start implementing SongSaver

---
+++ src/main/java/com/robothaver/mp3reorder/mp3_viewer/song/saver/SongSaverImpl.java
@@ -0,0 +1,18 @@
+package com.robothaver.mp3reorder.mp3_viewer.song.saver;
+
+import com.robothaver.mp3reorder.mp3_viewer.song.domain.Song;
+
+import java.nio.file.Path;
+import java.util.List;
+
+public class SongSaverImpl implements SongSaver {
+    @Override
+    public void SaveSongs(List<Song> songs) {
+
+    }
+
+    @Override
+    public void SaveAsSongs(List<Song> songs, Path savePath) {
+
+    }
+}
